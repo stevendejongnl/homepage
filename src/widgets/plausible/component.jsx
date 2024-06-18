fddd55d681c5f51579db06581ea5d33085eee854
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-18 22:15:46 +0200

    dot notation

--- src/widgets/plausible/component.jsx
+++ src/widgets/plausible/component.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "next-i18next";
+
 import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
 import useWidgetAPI from "utils/proxy/use-widget-api";
@@ -9,8 +10,6 @@ export default function Component({ service }) {
   const { data: realtimeVisitors, error: realtimeVisitorsError } = useWidgetAPI(widget, "realtimeVisitors");
   const { data: aggregate, error: aggregateError } = useWidgetAPI(widget, "aggregate");
 
-  console.log(realtimeVisitors, aggregate['results']);
-
   if (realtimeVisitorsError || aggregateError || false) {
     const finalError = realtimeVisitorsError ?? aggregateError ?? false;
     return <Container service={service} error={finalError} />;
@@ -20,20 +19,20 @@ export default function Component({ service }) {
     <Container service={service}>
       <Block label="plausible.realtimeVisitors" value={t("common.number", { value: realtimeVisitors })}/>
 
-      {aggregate?.['results']?.['bounce_rate'] ? (
-        <Block label="plausible.aggregateBouncerate" value={t("common.number", { value: aggregate['results']['bounce_rate']['value'] })}/>
+      {aggregate?.results?.bounce_rate ? (
+        <Block label="plausible.aggregateBounceRate" value={t("common.number", { value: aggregate.results.bounce_rate.value })}/>
       ) : null}
 
-      {aggregate?.['results']?.['pageviews'] ? (
-        <Block label="plausible.aggregatePageviews" value={t("common.number", { value: aggregate['results']['pageviews']['value'] })}/>
+      {aggregate?.results?.pageviews ? (
+        <Block label="plausible.aggregatePageviews" value={t("common.number", { value: aggregate.results.pageviews.value })}/>
       ) : null}
 
-      {aggregate?.['results']?.['visit_duration'] ? (
-        <Block label="plausible.aggregateVisitDuration" value={t("common.number", { value: aggregate['results']['visit_duration']['value'] })}/>
+      {aggregate?.results?.visit_duration ? (
+        <Block label="plausible.aggregateVisitDuration" value={t("common.number", { value: aggregate.results.visit_duration.value })}/>
       ) : null}
 
-      {aggregate?.['results']?.['visitors'] ? (
-        <Block label="plausible.aggregateVisitors" value={t("common.number", { value: aggregate['results']['visitors']['value'] })}/>
+      {aggregate?.results?.visitors ? (
+        <Block label="plausible.aggregateVisitors" value={t("common.number", { value: aggregate.results.visitors.value })}/>
       ) : null}
     </Container>
   );
